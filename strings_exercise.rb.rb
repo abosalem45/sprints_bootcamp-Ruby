8c4a78257fd14f7770035d1381942ffa18a983a2
abosalem45 <35200097+abosalem45@users.noreply.github.com>
2022-01-26 16:25:39 +0200

    Add files via upload

---
+++ strings_exercise.rb.rb
@@ -0,0 +1,12 @@
+String input_string = "Hello Ruby World"
+p input_string.split(' ')[1..-1].join(' ')
+p input_string.insert(0, "Welcome to ")
+input_string["World"] = "Ahmed"
+p input_string
+input_string["Ahmed"]= "\"Ahmed\"" 
+puts input_string
+String updated_string = "Hello Ruby World".insert(10, ",")
+p updated_string.insert(12," ")
+p "Hello Ruby World".size
+p "Hello Ruby World".count(" ")
+p "Hello Ruby World".gsub(" ", "-") 
